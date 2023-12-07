1cffa50168dfbcaedef0002ce22c04d6b85ffb99
Michelle Hu Ynh Tin <116173980+michelle264@users.noreply.github.com>
2023-12-07 23:15:37 +0800

    update login and register

--- app/src/main/java/com/example/drugrecoveryapp/EditProfile.java
+++ app/src/main/java/com/example/drugrecoveryapp/EditProfile.java
@@ -10,7 +10,5 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-
-
     }
 }
--- app/src/main/java/com/example/drugrecoveryapp/Register.java
+++ app/src/main/java/com/example/drugrecoveryapp/Register.java
@@ -23,6 +23,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -33,7 +34,7 @@ import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private EditText UserName,UserEmail,UserPassword,UserConfrimPassword;
+    private EditText UserName, UserEmail, UserPassword, UserConfrimPassword;
     private ProgressDialog loadingBar;
     private Button CreateAccountButton;
     private User user;
@@ -41,22 +42,20 @@ public class Register extends AppCompatActivity {
     private TextView bottomLogin;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        mAuth= FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-        UserName=(EditText) findViewById(R.id.register_username);
-        UserEmail=(EditText) findViewById(R.id.register_email);
+        UserName = (EditText) findViewById(R.id.register_username);
+        UserEmail = (EditText) findViewById(R.id.register_email);
 //        UserPhoneNumber=(EditText) findViewById(R.id.register_phonenumber) ;
-        UserPassword=(EditText) findViewById(R.id.register_password);
-        UserConfrimPassword=(EditText) findViewById(R.id.register_cofirmpassword);
-        CreateAccountButton=(Button) findViewById(R.id.register_create_account);
+        UserPassword = (EditText) findViewById(R.id.register_password);
+        UserConfrimPassword = (EditText) findViewById(R.id.register_cofirmpassword);
+        CreateAccountButton = (Button) findViewById(R.id.register_create_account);
 
-        loadingBar=new ProgressDialog(this);
+        loadingBar = new ProgressDialog(this);
         CreateAccountButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -111,30 +110,31 @@ public class Register extends AppCompatActivity {
             }
         });
     }
-    public void CreateNewAccount(){
+
+    public void CreateNewAccount() {
 
         //get text from user input
-        String username=UserName.getText().toString();
-        String email=UserEmail.getText().toString();
+        String username = UserName.getText().toString();
+        String email = UserEmail.getText().toString();
 //        String phoneNumber=UserPhoneNumber.getText().toString();
-        String password=UserPassword.getText().toString();
-        String confirmPassword=UserConfrimPassword.getText().toString();
+        String password = UserPassword.getText().toString();
+        String confirmPassword = UserConfrimPassword.getText().toString();
 //        String mobileRegex = "\\+[0-9]{2}[6-9][0-9]{9}";
 //        Matcher mobileMatcher;
 //        Pattern mobilePattern=Pattern.compile(mobileRegex);
 //        mobileMatcher=mobilePattern.matcher(phoneNumber);
 
         //prompt user to key in the information if the column is still empty
-        if(TextUtils.isEmpty(username)){
-            Toast.makeText(Register.this,"Please insert your username...",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(Register.this, "Please insert your username...", Toast.LENGTH_LONG).show();
             UserName.setError("Username is required");
             UserName.requestFocus();
 
-        }else if(TextUtils.isEmpty(email)) {
+        } else if (TextUtils.isEmpty(email)) {
             Toast.makeText(this, "Please insert your email...", Toast.LENGTH_LONG).show();
             UserEmail.setError("Email is required");
             UserEmail.requestFocus();
-        }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             Toast.makeText(Register.this, "Please re-enter your email", Toast.LENGTH_LONG).show();
             UserEmail.setError("Valid email is required");
             UserEmail.requestFocus();
@@ -154,70 +154,80 @@ public class Register extends AppCompatActivity {
 //            UserPhoneNumber.setError("Mobile No. is not valid");
 //            UserPhoneNumber.requestFocus();
 
-        }else if(TextUtils.isEmpty(password)) {
+        } else if (TextUtils.isEmpty(password)) {
             Toast.makeText(this, "Please insert your password...", Toast.LENGTH_LONG).show();
             UserPassword.setError("Password is required");
             UserPassword.requestFocus();
 
-        }else if(password.length()<6){
-            Toast.makeText(Register.this,"Password should be at least 6 digits",Toast.LENGTH_LONG).show();
+        } else if (password.length() < 6) {
+            Toast.makeText(Register.this, "Password should be at least 6 digits", Toast.LENGTH_LONG).show();
             UserPassword.setError("Password too weak");
             UserPassword.requestFocus();
 
-        }else if(TextUtils.isEmpty(confirmPassword)){
-            Toast.makeText(this,"Please insert your password...",Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(confirmPassword)) {
+            Toast.makeText(this, "Please insert your password...", Toast.LENGTH_LONG).show();
             UserConfrimPassword.setError("Password is required");
             UserConfrimPassword.requestFocus();
 
-        }else if(!(password.equals(confirmPassword))) {
+        } else if (!(password.equals(confirmPassword))) {
             Toast.makeText(this, "Your password do not match with your confirm password...", Toast.LENGTH_LONG).show();
-            UserConfrimPassword.setError("Password Confirmation is required");
+            UserConfrimPassword.setError("Please recheck password confirmation.");
             UserConfrimPassword.requestFocus();
 
             //Clear the entered passwords
             UserPassword.clearComposingText();
             UserConfrimPassword.clearComposingText();
 
-        }else{
-            //all the columns are filled,start creating new account
-
-            //loading bar message
-            loadingBar.setTitle("Creating New Account...");
-            loadingBar.setMessage("Please wait, we are creating your new account...");
-            loadingBar.show();
-            loadingBar.setCanceledOnTouchOutside(true);
-            //use email and password to create
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        } else {
+            // Check if the email is already registered
+            mAuth.fetchSignInMethodsForEmail(email).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
                 @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    // Tell the user whether they were successful in creating an account or not
+                public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
                     if (task.isSuccessful()) {
-                        Toast.makeText(Register.this, "You are authenticated successfully...", Toast.LENGTH_SHORT).show();
-                        loadingBar.dismiss();
+                        SignInMethodQueryResult result = task.getResult();
+                        List<String> signInMethods = result.getSignInMethods();
+                        if (signInMethods != null && signInMethods.size() > 0) {
+                            // Email is already registered
+                            Toast.makeText(Register.this, "This email is already registered. Please use a different email.", Toast.LENGTH_LONG).show();
+                        } else {
+                            // All the columns are filled, start creating a new account
+                            // ... Existing code for loading bar and account creation
 
-                        // Create a new User object with the provided information
-                        user = new User(FirebaseAuth.getInstance().getUid(), username, email, "", "");
+                            // Use email and password to create the account
+                            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    // Tell the user whether they were successful in creating an account or not
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(Register.this, "You are authenticated successfully...", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
 
-                        // Store the user object in the "Users" node of the Firebase Realtime Database
-                        FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).setValue(user);
+                                        // Create a new User object with the provided information
+                                        user = new User(FirebaseAuth.getInstance().getUid(), username, email, "", "");
 
-                        // Start the Setup_Activity
-                        Intent mainIntent = new Intent(Register.this, Setup.class);
-                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(mainIntent);
-                        finish();
+                                        // Store the user object in the "Users" node of the Firebase Realtime Database
+                                        FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).setValue(user);
+
+                                        // Start the Setup_Activity
+                                        Intent mainIntent = new Intent(Register.this, Setup.class);
+                                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(mainIntent);
+                                        finish();
+                                    } else {
+                                        // Display the error message to the user
+                                        String message = task.getException().getMessage();
+                                        Toast.makeText(Register.this, "Error Occurred: " + message, Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+                                    }
+                                }
+                            });
+                        }
                     } else {
-                        // Display the error message to the user
-                        String message = task.getException().getMessage();
-                        Toast.makeText(Register.this, "Error Occurred: " + message, Toast.LENGTH_SHORT);
-                        loadingBar.dismiss();
+                        // Error occurred while checking email existence
+                        Toast.makeText(Register.this, "Error checking email existence. Please try again.", Toast.LENGTH_LONG).show();
                     }
                 }
-
             });
         }
-
     }
-
-
 }
